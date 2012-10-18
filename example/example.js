faa7d819ddee93970ece99f5c3018da4601dd5c7
ayalgelles <ayalgelles@gmail.com>
2012-10-18 22:55:32 +0200

    example uses server image when no db image

--- example/example.js
+++ example/example.js
@@ -1,17 +1,10 @@
 images = new Meteor.Collection("images");
 
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to example.";
+  Template.hello.fbDebugLink = function () {
+    return "https://developers.facebook.com/tools/debug/og/object?q=" + encodeURIComponent(location.href);
   };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
 }
 
 if (Meteor.is_server) {
@@ -23,9 +16,10 @@ if (Meteor.is_server) {
     Meteor.headly.config({tagsForRequest: function(req, callback) {
 			      var url = __meteor_bootstrap__.require('url'); 
 			      var parts = url.parse(req.url).pathname.split('/'); //using url to determine og:title 
-			      var image = images.findOne({name: parts[1]}); // showing we can run db-access code in the headly callback
+			      var image = images.findOne({name: parts[1]}); // we can run db-access code in the headly callback
 			      callback('<meta property="og:title" content="' + parts[1] + ' - ' + parts[2] + '" />\n'
-				       + '<meta property="og:image" content="' + image.url + '" />\n');
+				       + '<meta property="og:image" content="' + 
+				       (image ? image.url : (('http://' + req.headers.host) + '/1.jpeg')) + '" />\n'); // image is either from db or server from our app
 			  }
 			 });
     
