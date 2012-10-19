a65bedbe8e1e5f16684b07d5d7825fba52270cf6
ayalgelles <ayalgelles@gmail.com>
2012-10-19 10:45:36 +0200

    indentation and tagsForRequest is no cps anymore

--- example/example.js
+++ example/example.js
@@ -1,9 +1,9 @@
 images = new Meteor.Collection("images");
 
 if (Meteor.is_client) {
-  Template.hello.fbDebugLink = function () {
-    return "https://developers.facebook.com/tools/debug/og/object?q=" + encodeURIComponent(location.href);
-  };
+    Template.hello.fbDebugLink = function () {
+        return "https://developers.facebook.com/tools/debug/og/object?q=" + encodeURIComponent(location.href);
+    };
 
 }
 
@@ -13,17 +13,17 @@ if (Meteor.is_server) {
     images.insert({name: 'headly', url: 'http://wiki.opscode.com/download/attachments/1180194/head_wire.jpg'});	
 
 
-    Meteor.headly.config({tagsForRequest: function(req, callback) {
-			      var url = __meteor_bootstrap__.require('url'); 
-			      var parts = url.parse(req.url).pathname.split('/'); //using url to determine og:title 
-			      var image = images.findOne({name: parts[1]}); // we can run db-access code in the headly callback
-			      callback('<meta property="og:title" content="' + parts[1] + ' - ' + parts[2] + '" />\n'
-				       + '<meta property="og:image" content="' + 
-				       (image ? image.url : (('http://' + req.headers.host) + '/1.jpeg')) + '" />\n'); // image is either from db or server from our app
-			  }
+    Meteor.headly.config({tagsForRequest: function(req) {
+	var url = __meteor_bootstrap__.require('url'); 
+	var parts = url.parse(req.url).pathname.split('/'); //using url to determine og:title 
+	var image = images.findOne({name: parts[1]}); // we can run db-access code in the headly callback
+	return '<meta property="og:title" content="' + parts[1] + ' - ' + parts[2] + '" />\n'
+	    + '<meta property="og:image" content="' + 
+	    (image ? image.url : (('http://' + req.headers.host) + '/1.jpeg')) + '" />\n'; // image is either from db or server from our app
+    }
 			 });
     
     Meteor.startup(function () {
-		       // code to run on server at startup
-		   });
+	// code to run on server at startup
+    });
 }
--- headly_server.js
+++ headly_server.js
@@ -1,35 +1,39 @@
 (function () {
-     if (!Meteor.headly) {	 
-	 Meteor.headly = {};
-     }
+    if (!Meteor.headly) {	 
+	Meteor.headly = {};
+    }
+    
+    if (!Meteor.headly._options) {
+	Meteor.headly._options = {};
+    }
+    
+    Meteor.headly.config = function(options) {
+	Meteor.headly._options = Meteor.headly._options;
+    };
+    
+    __meteor_bootstrap__.app
+	.use(function(req, res, next) {
+	    if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		Fiber(function () {
+		    
+                    if (!Meteor.headly._options.tagsForRequest) {
+                        throw Error('headly was not configured with tagsForRequest function.');
+                    }
 
-     if (!Meteor.headly._options) {
-	 Meteor.headly._options = {};
-     }
+		    var head = Meteor.headly._options.tagsForRequest(req);
+                    
+		    if (head) {
+			res.write('<head>');
+			res.write(head);
+			res.write('</head>');
+		    }
+		    res.end();
 
-     Meteor.headly.config = function(options) {
-	 Meteor.headly._options = options || Meteor.headly._options;
-     };
-
-     __meteor_bootstrap__.app
-	 .use(function(req, res, next) {
-		  if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
-		      res.writeHead(200, {'Content-Type': 'text/html'});
-		      Fiber(function () {
-				Meteor.headly._options.tagsForRequest && 
-				    Meteor.headly._options.tagsForRequest(req, 
-								function(head) {
-								    if (head) {
-									res.write('<head>');
-									res.write(head);
-									res.write('</head>');
-								    }
-								    res.end();
-								});
-			    }).run();
-		  }
-		  else {
-		      next();   
-		  }
-	      });
- })();
+		}).run();
+	    }
+	     else {
+		next();   
+	    }
+	});
+})();
